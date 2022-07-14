88a0c234c1aa8e46d447fdd1ea50aae05604dbe4
Moringa School <geogrey.muiruri@student.moringaschool.comgit config --global user.name greg-mbugua>
2022-07-15 00:24:44 +0300

    calculates amount earned in january

--- index.js
+++ index.js
@@ -35,7 +35,7 @@ months = ["January","February","March",
                            +parseFloat(field3.value)+parseFloat(field4.value)) * 45 * 7;
         
             if(!isNaN(answer)){
-               weeklyIncome.innerHTML = "Your weekly income will be <br> ksh "+answer;
+               weeklyIncome.innerHTML = "Expected weekly income will be <br> ksh "+answer;
             }
             // Yearly income
             const response = (parseFloat(field1.value)+parseFloat(field2.value)
@@ -44,4 +44,27 @@ months = ["January","February","March",
             if(!isNaN(response)){
                yearlyIncome.innerHTML = "Your yearly income will be <br> ksh "+response;
             }
-        }      
+        }
+        //income per month on a leap year
+        function incomeMonthly(){
+            
+            // Initialising individual months variable
+    const january  = document.getElementById("january");
+    const february = document.getElementById("february");
+    const march    = document.getElementById("march");
+    const april    = document.getElementById("april");
+    const may      = document.getElementById("may");
+    const june     = document.getElementById("june");
+    const july     = document.getElementById("july");
+    const august   = document.getElementById("august");
+    const september= document.getElementById("september");
+    const october  = document.getElementById("october");
+    const november = document.getElementById("november");
+    const december = document.getElementById("december");
+        }  
+        //compute january total income using the brookside rate which is 45 shillings per litre
+        const jan = (parseFloat(field1.value))+parseFloat(field2.value)
+                    +parseFloat(field3.value)+parseFloat(field4.value) * 45 * 31;
+        if(!isNaN(jan)){
+                        january.innerHTML = "Income for January is "+jan;
+ }    
